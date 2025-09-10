43ffd4722c9e6ae95b62887e8e058d888f14f183
ikunstoppable <wangwang357@yeah.net>
2025-09-10 21:11:45 +0800

    first commit

---
+++ initial.py
@@ -0,0 +1,50 @@
+# python 实现对纳斯达克指数定投收益计算
+import pandas as pd
+
+# 带千分位字符串转浮点数
+def str_to_float(s):
+  return float(s.replace(',', ''))
+
+# 计算出每日固定投入，计算出区间收益
+# 输入：开始日期xxxx/xx/xx，和时间跨度（几年），定投金额
+# 输出：定投后总金额，定投总成本，定投利润
+def fixed_calculate_profit(start_date, time_span, invest_amount):
+  # df = pd.read_csv("./files/allDate.csv")
+  # 获取开始日期之前的交易日数据
+  df = pd.read_csv("./files/allDate.csv")
+  end_date = str(int(start_date.split('/')[0]) + int(time_span)) + '/' + start_date.split('/')[1] + '/' + start_date.split('/')[2]
+  df = df[(df['日期'] >= start_date) & (df['日期'] < end_date)]
+  print(end_date)
+  rows, columns  = df.shape
+  print('总共行数', rows)
+  end = df2024.iloc[0]
+  endprice = end['收盘']
+  profit_sum = 0
+  # 总投入成本
+  total_cost = invest_amount * rows
+  # 倒序遍历2024年交易日数据，计算每天的收益  
+  for i in range(rows - 1, -1, -1):
+    current_row = df.iloc[i]
+    current_price = current_row['收盘']
+    current_profit = invest_amount + (str_to_float(endprice) - str_to_float(current_price)) / str_to_float(current_price) * invest_amount
+    profit_sum += current_profit
+
+  print(f'{start_date} - {end_date}定投后总金额：', profit_sum)
+  print(f'{start_date} - {end_date}定投成本总金额：', total_cost)
+  print(f'{start_date} - {end_date}定投收益额：', profit_sum - total_cost)
+
+fixed_calculate_profit('2021/01/01', 4, 200)
+
+# 计算随机起始日期[1986年，]，固定时间跨度，固定定投金额，计算收益的分布，即随机调用fixed_calculate_profit函数
+# 输入：随机次数，开始日期范围，时间跨度，定投金额
+# 输出：定投后总金额，定投总成本，定投利润
+def random_start_date_calculate_profit(random_count, start_date_range, time_span, invest_amount):
+  profit_list = []
+  for i in range(random_count):
+    
+# 随机生成日期范围内的起始日期
+def random_start_date(start_date_range):
+  start_year = random.randint(start_date_range[0], start_date_range[1])
+  start_month = random.randint(1, 12)
+  start_day = random.randint(1, 28)
+    
